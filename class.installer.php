d5b58076e4bc22bb0b19befe5a6cff6267c1b6d0
Andrew Champ <atmydarkest@gmail.com>
2016-02-26 12:10:54 -0500

    Added links for framework repository archives

--- class.installer.php
+++ class.installer.php
@@ -1,16 +1,50 @@
 <?php
-	
+
+	/**
+	 *	EXAMPLES OF USE
+	 *************************************
+	 *
+	 *	Download and unzip your custom framework.  Then create the database connection file.
+	 *
+	 *	$install = new installer();
+	 *	$install->download('http://your-domain.com/framework.zip');
+	 *	$install->unpack();
+	 *	$cred = array(
+	 *		'DB_HOST' => 'localhost', 
+	 *		'DB_DATABASE' => 'user_db12', 
+	 *		'DB_USER' => 'user_kewl', 
+	 *		'DB_PASSWORD' => 'abc123456'
+	 *	);
+	 *	$install->create($cred, 'config.php');
+	 *	
+	 *	
+	 *	...OR...
+	 *	
+	 *	
+	 *	Download and unzip Wordpress (or a framework of your choosing).
+	 *
+	 *	$install = new installer($url);
+	 *	$install->download($install->framework['wordpress']);
+	 *	$install->unpack();
+	 *	 
+	 **/
+	 
+
 	class installer{
 		
-		public $zipUrl;
 		public $installer;
+		public $framework = array(
+			'wordpress' => 'https://wordpress.org/latest.zip',
+			'joomla' => 'https://github.com/joomla/joomla-cms/releases/download/3.4.8/Joomla_3.4.8-Stable-Full_Package.zip',
+			'druplal' => 'https://ftp.drupal.org/files/projects/drupal-8.0.4.zip',
+			'concrete5' => 'https://www.concrete5.org/download_file/-/view/85780/',
+			'opencart' => 'https://codeload.github.com/opencart/opencart/zip/2.1.0.2'
+		);
+
 		
-		public function __construct($_zipUrl){
+		public function __construct(){
 			$this->installer = 'install-'.time().'.zip';
 			$this->zipUrl = $_zipUrl;
-			
-			$this->download();
-			$this->unpack();
 		}
 		
 		
@@ -22,10 +56,10 @@
 		}
 		
 		
-		public function download(){
-			if($this->checkFile($this->zipUrl)):
+		public function download($url){
+			if($this->checkFile($url)):
 				$fp = fopen($this->installer, 'w');
-				$ch = curl_init($this->zipUrl);
+				$ch = curl_init($url);
 				curl_setopt($ch, CURLOPT_FILE, $fp);
 				$data = curl_exec($ch);
 				curl_close($ch);
@@ -51,6 +85,17 @@
 				print __CLASS__.': Installer zip does not exist.'."\n";
 			endif;
 		}
+		
+		
+		public function create($inputs=array(), $filename='db_config.php'){
+			$config = '<?php'."\n\n";
+			foreach($inputs as $k=>$v):
+				$config .= "\tdefine('".strtoupper($k)."', '".$v."');\n";
+			endforeach;
+			$config .= "\n".'?>';
+			file_put_contents($filename, $config);
+		}
+		
 		
 	}
 
